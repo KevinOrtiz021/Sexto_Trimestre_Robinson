47507efbb4936093b010ca6b16a7a258d695fe89
ArenasKevin021@gmail.com <arenaskevin021@gmail.com>
2025-09-21 20:53:45 -0500

    logica

--- Logica/ejemplo1.php
+++ Logica/ejemplo1.php
@@ -9,6 +9,20 @@
             font-family: Arial, sans-serif;
             margin: 20px;
         }
+        nav a {
+            text-decoration: none;
+            display: inline-block;
+            padding: 10px;
+            background-color: #4CAF50;
+            border-radius: 50px;
+            margin-bottom: 20px;
+            color: white;
+            font-size: 20px;
+            text-align: center;
+            width: 30px;
+            height: 30px;
+            line-height: 30px;
+        }
         h2 {
             color: #333;
         }
@@ -39,6 +53,11 @@
     </style>
 </head>
 <body>
+    <nav>
+        <a href="index.php">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+        </a>
+    </nav>
     <h2> ALGORITMOS SUMAR DOS NUMEROS </h2>
     <form method="post">
         Numero 1: <input type="number" name="n1" required><br><br>
--- Logica/ejemplo17.php
+++ Logica/ejemplo17.php
@@ -10,6 +10,20 @@
             font-family: Arial, sans-serif;
             margin: 20px;
         }
+        nav a {
+            text-decoration: none;
+            display: inline-block;
+            padding: 10px;
+            background-color: #4CAF50;
+            border-radius: 50px;
+            margin-bottom: 20px;
+            color: white;
+            font-size: 20px;
+            text-align: center;
+            width: 30px;
+            height: 30px;
+            line-height: 30px;
+        }
         h3 {
             color: #555;
             text-align: center;
@@ -61,6 +75,11 @@
 </head>
 
 <body>
+    <nav>
+        <a href="index.php">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+        </a>
+    </nav>
     <h3>Login de un usuario</h3>
     <form method="post">
         <label for="usuario">Ingrese su usuario:</label>
